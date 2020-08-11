8a6f6f5238e78ecc71572ae3b2b12a8b09fd0985
Dimamik <dimamik2020@gmail.com>
2020-08-11 17:43:30 +0300

    added resources...

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ provider "google" {
   zone        = "us-central1-c"
 }
 
-resource "google_compute_instance" "default" {
-  name         = "test"
+resource "google_compute_instance" "terraform-task-instance" {
+  name         = "terraform-task"
   machine_type = "g1-small"
   zone         = "europe-west3-a"
   tags = ["dimamik", "terraformed"]
@@ -16,4 +16,28 @@ resource "google_compute_instance" "default" {
       image = "ubuntu-1804-bionic-v20200807"
     }
   }
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+
+resource "google_compute_firewall" "terraform-task-firewall" {
+  name    = "terraform-task-firewall"
+  network = google_compute_network.default.name
+
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80","443"]
+        }
+
+  source_tags = ["web"]
+}
+
+resource "google_compute_network" "default" {
+  name = "terraform-task-network"
 }
