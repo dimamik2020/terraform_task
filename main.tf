3cb5e20d83b73ce658fe967fa6c59536e30bf616
Dimamik <dimamik2020@gmail.com>
2020-08-11 16:55:52 +0300

    Added first gcp resource

--- main.tf
+++ main.tf
@@ -4,3 +4,15 @@ provider "google" {
   region      = "us-central1"
   zone        = "us-central1-c"
 }
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "g1-small"
+  zone         = "europe-west3-a"
+  tags = ["dimamik", "terraformed"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20200807"
+    }
+  }
