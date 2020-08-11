c020fa9a20628a56d8190f5578c904105900290e
Dimamik <dimamik2020@gmail.com>
2020-08-11 16:41:15 +0300

    Added references to credentials and project id.

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
-resource "" "" {
-
+provider "google" {
+  credentials = file("credentials.json")
+  project     = "fleet-goal-286112"
+  region      = "us-central1"
+  zone        = "us-central1-c"
 }
