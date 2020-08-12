763b7619fcdd8bc431f47f7fcc39346c368ae35a
Dimamik <dimamik2020@gmail.com>
2020-08-12 11:59:13 +0300

    Added network, rules

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "google_compute_instance" "terraform-task-instance" {
   name         = "terraform-task-instance"
   machine_type = "g1-small"
   zone         = "europe-west3-a"
-  tags = ["dimamik", "terraformed"]
+  tags = ["terraform-task"]
 
   boot_disk {
     initialize_params {
@@ -22,23 +22,23 @@ resource "google_compute_instance" "terraform-task-instance" {
     }
   }
   network_interface {
-  network = "default"
-
+  #network = "default"
+  network = google_compute_network.terraform-task-network.self_link
         access_config {
         nat_ip = google_compute_address.static.address
                       }
                     }
 
-metadata  {
-       ssh-keys = "ubuntu:${file("c:/User/Dima/.ssh/id_rsa.pub")}"
+metadata = {
+       ssh-keys = "ubuntu:${file("id_rsa.pub")}"
           }
 
 
 }
 
-resource "google_compute_firewall" "terraform-task-firewall" {
-  name    = "terraform-task-firewall"
-  network = google_compute_network.default.name
+resource "google_compute_firewall" "terraform-task-firewall-web" {
+  name    = "terraform-task-firewall-web"
+  network = google_compute_network.terraform-task-network.name
 
 
   allow {
@@ -46,15 +46,24 @@ resource "google_compute_firewall" "terraform-task-firewall" {
     ports    = ["80","443"]
         }
 
+}
+
+resource "google_compute_firewall" "terraform-task-firewall-ssh" {
+  name    = "terraform-task-firewall-ssh"
+  network = google_compute_network.terraform-task-network.name
+
+
   allow {
     protocol = "tcp"
     ports    = ["22"]
 
         }
 
-  source_tags = ["web"]
+  source_ranges = ["82.207.102.102"]
 }
 
-resource "google_compute_network" "default" {
+
+resource "google_compute_network" "terraform-task-network" {
   name = "terraform-task-network"
+  auto_create_subnetworks = "true"
 }
